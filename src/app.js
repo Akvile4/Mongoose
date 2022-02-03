ddb2237d14e6f49963f713b1f76741369bd4fdec
Akvile4 <aqua0818@gmail.com>
2022-02-03 22:32:41 +0200

    added update and delete methods

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ require('./db/connection');
 const mongoose = require('mongoose');
 
 const yargs = require('yargs/yargs');
-const { addMovie, list } = require('./film/filmMethods');
+const { addMovie, list, update, deleteOne } = require('./film/filmMethods');
 const {hideBin} = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
+
 
 const app = async () => {
     if (argv.add) {
@@ -14,12 +15,29 @@ const app = async () => {
         })
     }
     else if (argv.list) {
-        list()
+        await list()
         console.log('list')
+    }
+    // else if (argv.update) {
+    //     await FilmModel.updateOne({
+    //         name: argv.title,
+    //         actor: argv.actor
+    //     })
+    //     console.log('Update happened');
+    // }
+    else if (argv.updateOne) {
+        await update();
+        console.log('We got the updates');
+    }
+    else if (argv.delete) {
+        await deleteOne();
+        console.log('DELETED!!!!')
     }
     else {
         console.log('wrong commamnd')
     }
+        // will close the connection so the terminal will stop running , but we have to await when we are calling any function
+    mongoose.connection.close();
 }
 
 app()
